13d246fe5dc95127e2764c6622b75d8b0819267d
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2022-07-05 12:07:48 +0200

    refactoring to module styles

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,17 +1,18 @@
 import { useRouter } from "next/router";
 
+import classes from '../../styles/MeetupDetail.module.css';
+
 import { DUMMY_LIST } from "..";
 
 export default function MeetupDetail() {
   const router = useRouter();
   const id = router.query.meetupId;
-  console.log(id);
   const meetup = DUMMY_LIST.find((item) => item.id === id);
 
   if (!meetup) return <h1>Meetup not found!</h1>
 
   return (
-    <div className="item">
+    <div className={classes.item}>
       <img src={meetup.image} />
       <span>{meetup.title}</span>
       <p className="description">{meetup.description}</p>
