995da8c8c66144a3548cfd554fa5df5e3ded8655
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2022-07-10 10:04:07 +0200

    removing dotenv

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig}  */
-if (process.env.NODE_ENV === 'production') {
-  require('dotenv').config()
-  throw new Error(process.env.DATABASE_URI)
-}
 
 const nextConfig = {
   reactStrictMode: true,
